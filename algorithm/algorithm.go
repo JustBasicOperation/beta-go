eab69b5f0b958e7ab176523d0b57bb41deebad12
faustzhao <faustzhao@tencent.com>
2022-07-10 17:16:32 +0800

    feat: 2022.07.10

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -4,14 +4,19 @@ import (
 	"sort"
 )
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int, 10)
-	for i := range nums {
-		tmp := target - nums[i]
-		if v, ok := m[nums[i]]; ok {
+	m := make(map[int]int, len(nums))
+	for i, n := range nums {
+		sub := target - n
+		if v, ok := m[n]; ok {
 			return []int{v, i}
 		}
-		m[tmp] = i
+		m[sub] = i
 	}
 	return []int{}
 }
@@ -53,4 +58,88 @@ func threeSum(nums []int) [][]int {
 		}
 	}
 	return res
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	resList := &ListNode{}
+	pHead := resList
+	carry := 0
+	// the characteristic of last list node is ptr != nil && ptr.Next = nil
+	for l1 != nil && l2 != nil {
+		v1 := l1.Val
+		v2 := l2.Val
+		var sum int
+		if carry > 0 {
+			sum = v1 + v2 + carry
+			carry = 0
+		} else {
+			sum = v1 + v2
+		}
+		carry = sum / 10
+		pHead.Next = &ListNode{
+			Val: sum % 10,
+		}
+		pHead = pHead.Next
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	for l1 != nil {
+		var sum int
+		pHead.Next = &ListNode{}
+		if carry > 0 {
+			sum = l1.Val + carry
+			carry = 0
+		} else {
+			sum = l1.Val
+		}
+		carry = sum / 10
+		pHead.Next.Val = sum % 10
+		pHead = pHead.Next
+		l1 = l1.Next
+	}
+	for l2 != nil {
+		var sum int
+		pHead.Next = &ListNode{}
+		if carry > 0 {
+			sum = l2.Val + carry
+			carry = 0
+		} else {
+			sum = l2.Val
+		}
+		carry = sum / 10
+		pHead.Next.Val = sum % 10
+		pHead = pHead.Next
+		l2 = l2.Next
+	}
+	if carry > 0 {
+		pHead.Next = &ListNode{
+			Val: carry,
+		}
+	}
+	return resList.Next
+}
+
+func lengthOfLongestSubstring(s string) int {
+	right, res := -1, 0
+	tmpMap := make(map[byte]bool, len(s))
+	for left := 0; left < len(s); left++ {
+		if left != 0 {
+			delete(tmpMap, s[left-1])
+		}
+		for right+1 < len(s) && !tmpMap[s[right+1]] {
+			tmpMap[s[right+1]] = true
+			right++
+		}
+		res = max(res, right+1-left)
+	}
+
+	return res
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	} else {
+		return j
+	}
 }
--- algorithm/algorithm_test.go
+++ algorithm/algorithm_test.go
@@ -1,26 +1,55 @@
 package algorithm
 
 import (
-	"reflect"
 	"testing"
 )
 
 func Test_twoSum(t *testing.T) {
+
+}
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
 	type args struct {
-		nums   []int
-		target int
+		s string
 	}
 	tests := []struct {
 		name string
 		args args
-		want []int
+		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				s: "bbbbb",
+			},
+			want: 1,
+		},
+		{
+			name: "case2",
+			args: args{
+				s: " ",
+			},
+			want: 1,
+		},
+		{
+			name: "case3",
+			args: args{
+				s: "abcabab",
+			},
+			want: 3,
+		},
+		{
+			name: "case3",
+			args: args{
+				s: "abc",
+			},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
 			}
 		})
 	}
