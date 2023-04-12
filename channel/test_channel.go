4e7fc275b1fa543c531a8e9cd924288f6456cf12
faustzhao <faustzhao@tencent.com>
2023-04-12 22:30:20 +0800

    20230412

--- channel/test_channel.go
+++ channel/test_channel.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"fmt"
+	"time"
 )
 
 func send(c chan<- int) {
@@ -14,5 +15,38 @@ func send(c chan<- int) {
 func recv(c <-chan int) {
 	for i := range c {
 		fmt.Println(i)
+	}
+}
+
+func talk(msg string, sleep int) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(sleep) * time.Millisecond)
+		}
+	}()
+	return ch
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for {
+			select {
+			case a := <-input1:
+				ch <- a
+			case b := <-input2:
+				ch <- b
+			}
+		}
+	}()
+	return ch
+}
+
+func test01() {
+	ch := fanIn(talk("A", 10), talk("B", 1000))
+	for range ch {
+		fmt.Printf("%q\n", <-ch)
 	}
 }
