47df36f08b5625e9079d1ac1f700d90200f2877f
faustzhao <faustzhao@tencent.com>
2024-01-21 18:25:43 +0800

    feat: 修复单测

--- algorithm/algorithm_test.go
+++ algorithm/algorithm_test.go
@@ -439,32 +439,6 @@ func TestReverseUrl(t *testing.T) {
 	}
 }
 
-func Test_reverseWords(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "valid case",
-			args: args{
-				s: "  hello  world  ",
-			},
-			want: "world hello",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := reverseWords(tt.args.s); got != tt.want {
-				t.Errorf("reverseWords() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestSimpleFourOperation(t *testing.T) {
 	type args struct {
 		expression []string
